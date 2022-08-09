1eb3283def33636182bbd23143a4490a47b2d045
emreeken183425 <emreeken1834@gmail.com>
2022-08-09 11:56:47 +0300

    ff

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import axios from 'axios';
 import { useEffect,useState} from 'react';
 import { map } from '@firebase/util';
+import MovieCard from '../components/MovieCard';
+import { AuthContext } from '../context/AuthContext';
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
@@ -10,7 +12,8 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 const Main=()=> {
  const [movies, setMovies] = useState([])
  const [loading, setLoading] = useState(false)
-
+ const [searchTerm, setSearchTerm] = useState("")
+const {currentUser}=useContext(AuthContext)
   useEffect(()=>{
     getMovies(FEATURED_API)
   },[])
@@ -18,23 +21,44 @@ const getMovies=(API)=>{
   setLoading(true)
 axios
 .get(API)
-.then((res)=>console.log(res.data.results))
+.then((res)=>setMovies(res.data.results))
 .then((err)=>console.log(err))
 .finally(()=>setLoading(false))
 }
+const handleSubmit=(e)=>{
+e.preventDefault()
 
+if(searchTerm && currentUser  ){
+  getMovies(SEARCH_API+searchTerm)
+} else if(!currentUser){
+  alert("Please log in to search movie..")
+}else{
+  alert("please enter a text")
+}
+}
 
   return (
+    <>
+    <form className='search' onSubmit={handleSubmit} >
+        <input type="search"
+           className='search-input'
+           placeholder='search a movie...'
+           onChange={(e)=> setSearchTerm(e.target.value)}
+           />
+          <button type='submit' >Search</button>
+    </form>
+  
     <div className='d-flex justify-content-center flex-wrap ' >
       {loading? (
         <div className='spinner-border text-primary m-5' role="status" >
 <span className='sr-only' >Loading...</span>
         </div>
       ):(
-        movies?.map((movie)=>null)
+        movies?.map((movie)=><MovieCard key={movie.id}{...movie} />)
       )
     }
     </div>
+    </>
   )
 }
 
